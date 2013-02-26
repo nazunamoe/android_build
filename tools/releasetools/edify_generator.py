267cb4f80bdd9b00067817d4da7958b0b7e1f9fe
Tom Marshall <tdm.code@gmail.com>
2013-02-26 20:37:04 +0000

    Remove unused RunConfig.
    This is unused.  It requires additional plumbing in several other files
    which does not exist.  And it has been removed from the CM repo as well.
    
    Change-Id: Id604c092758bb464c4306086a4d873159f0724ef

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -101,11 +101,6 @@ class EdifyGenerator(object):
            ");")
     self.script.append(self._WordWrap(cmd))
 
-  def RunConfig(self, command):
-    self.script.append('package_extract_file("system/bin/modelid_cfg.sh", "/tmp/modelid_cfg.sh");')
-    self.script.append('set_perm(0, 0, 0777, "/tmp/modelid_cfg.sh");')
-    self.script.append(('run_program("/tmp/modelid_cfg.sh", "%s");' % command))
-
   def RunBackup(self, command):
     self.script.append('package_extract_file("system/bin/backuptool.sh", "/tmp/backuptool.sh");')
     self.script.append('package_extract_file("system/bin/backuptool.functions", "/tmp/backuptool.functions");')
